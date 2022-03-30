c867c1b5b778362dfd4046b05f00f473dd0e7e21
Shelby Trodden <shelby.trodden@gmail.com>
2022-03-29 21:07:55 -0500

    add html site and subtract, multiple, and divide function

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,26 @@ function add(number1, number2) {
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
+const result = add(number1, number2);
+alert(result);
 
-function subtract()
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+const resultSub = subtract(number1, number2);
+alert(result);
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+const resultDiv = divide(number1, number2);
+alert(result);
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+const resultMul = multiply(number1, number2);
+alert(results);
