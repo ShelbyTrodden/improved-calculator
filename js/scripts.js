096522b8f417bb2a3104ed220ee62ea7dec5bd3b
Shelby Trodden <shelby.trodden@gmail.com>
2022-04-03 17:29:40 -0500

    initial UI and business logic and css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,59 @@
+// Business logic:
+
 function add(number1, number2) {
   return number1 + number2;
 }
-
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
 
 function subtract(number1, number2) {
   return number1 - number2;
 }
 
-const resultSub = subtract(number1, number2);
-alert(result);
+function multiply(number1, number2) {
+  return number1 * number2;
+}
 
 function divide(number1, number2) {
   return number1 / number2;
 }
 
-const resultDiv = divide(number1, number2);
-alert(result);
+// UI logic:
 
-function multiply(number1, number2) {
-  return number1 * number2;
-}
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
 
-const resultMul = multiply(number1, number2);
-alert(results);
+$(document).ready(function() {
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#output1").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#mul1").val());
+    const number2 = parseInt($("#mul2").val());
+    const result = multiply(number1, number2);
+    $("#output2").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#div1").val());
+    const number2 = parseInt($("#div2").val());
+    const result = divide(number1, number2);
+    $("#output3").text(result);
+  });
+});
