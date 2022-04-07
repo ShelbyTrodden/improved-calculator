f2c0e285146cfbabf072c06c6c0d2afdeade5f8d
Shelby Trodden <shelby.trodden@gmail.com>
2022-04-06 20:53:04 -0500

    allow user to select with math funciton will be performed via radio button

--- js/scripts.js
+++ js/scripts.js
@@ -19,41 +19,21 @@ function divide(number1, number2) {
 // UI logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
-  });
-});
-
-$(document).ready(function() {
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#sub1").val());
-    const number2 = parseInt($("#sub2").val());
-    const result = subtract(number1, number2);
-    $("#output1").text(result);
-  });
-});
-
-$(document).ready(function() {
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#mul1").val());
-    const number2 = parseInt($("#mul2").val());
-    const result = multiply(number1, number2);
-    $("#output2").text(result);
-  });
-});
-
-$(document).ready(function() {
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#div1").val());
-    const number2 = parseInt($("#div2").val());
-    const result = divide(number1, number2);
-    $("#output3").text(result);
   });
 });
